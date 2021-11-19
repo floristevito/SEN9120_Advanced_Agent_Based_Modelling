aebde2ae87ede0248f6b64049d476a2ef0f7b870
Floris Boendermaker <floris.boendermaker@gmail.com>
2021-11-19 11:31:14 +0100

    Added chargers (pick one)

--- model/components.py
+++ model/components.py
@@ -15,16 +15,42 @@ class Vehicle(EmtAgent):
     
     def setup(self):
         self.energy = 5
+        self.status = None
+        self.charger = None
+        self.charge_time = None
         
     def drive(self):
+        self.status = 'driving'
         self.energy -= 1
+    
+    def init_charge(self):
+        # check if charger left with energy
+        available_chargers = self.model.chargers.select(self.model.chargers.current_capacity > 0)
+        if available_chargers:
+            self.charger = available_chargers.random()
+            self.charger.add_car()
+            self.status = 'charging'
+            self.charge_time = 3
+        else:
+            self.status = 'waiting for charger'
+
+    def end_charge(self):
+        self.charger.remove_car()
+        self.status = None
     
     def charge(self):
         self.energy += 1
     
     def step(self):
-        self.energy -= 1
-        print('Vehicle {} has {} energy left'.format(self.id, self.energy))
+        if self.energy > 0:
+            self.drive()
+        else:
+            self.init_charge()
+            if self.status == 'charging' and self.charge_time > 0:
+                self.charge()
+            elif self.charge_time == 0:
+                self.end_charge()
+        print('Vehicle {} is {} and has {} energy left'.format(self.id, self.status, self.energy))
     
 class Charger(EmtAgent):
     
