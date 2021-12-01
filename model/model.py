cf95286dfb10e48a8b5c8567d22e0af537084ce2
Floris Boendermaker <39157834+floristevito@users.noreply.github.com>
2021-12-01 09:12:03 +0100

    comments + remove unneeded counter

--- model/model.py
+++ model/model.py
@@ -11,32 +11,34 @@ Main model block
 class EmtAgentsModel(ap.Model):
     """Main model that simulates electric vehicles"""
     def setup(self):
+        
         # generate the manicipalities according to data prep file
         self.OD = generate_OD(self.p.g)
         self.municipalities_data = pd.read_csv('../data/gemeenten.csv').set_index('GM_CODE')
         self.municipalities = ap.AgentList(self, len(self.OD), Municipality)
-        counter = 0 
-        for key, value in self.OD.items():
-            self.municipalities.id[counter] = key
-            self.municipalities.name[counter] = self.municipalities_data.loc[key, 'GM_NAAM']
-            self.municipalities.OD[counter] = value
-            self.municipalities.inhabitants[counter] = self.municipalities_data.loc[key, 'AANT_INW']
-            self.municipalities.number_EVs[counter] = round(self.p.percentage_ev * self.municipalities.inhabitants[counter])
-            counter += 1
+        # give the right properties to every municipality according to data prep file
+        for index, (key, value) in enumerate(self.OD.items()):
+            self.municipalities.id[index] = key
+            self.municipalities.name[index] = self.municipalities_data.loc[key, 'GM_NAAM']
+            self.municipalities.OD[index] = value
+            self.municipalities.inhabitants[index] = self.municipalities_data.loc[key, 'AANT_INW']
+            self.municipalities.number_EVs[index] = round(self.p.percentage_ev * self.municipalities.inhabitants[index])
         
         # generate EV's 
         self.EVs = ap.AgentList(self, sum(self.municipalities.number_EVs), EV)
-        counter = 0
+        index = 0 # keeps track of the EV index
+        # give the right properties to every EV according to the data prep file
         for mun in self.municipalities:
             for ev in range(mun.number_EVs):
-                self.EVs.home_location[counter] = mun.name
+                self.EVs.home_location[index] = mun.name
                 mapped_dest = mun.OD.sample(1, weights='p_flow')
-                self.EVs.work_lococation_id[counter] = mapped_dest['destination_id'].iloc[0]
-                self.EVs.work_location_name[counter] = self.municipalities_data.loc[self.EVs.work_lococation_id[counter], 'GM_NAAM']
-                self.EVs.commute_distance[counter] = mapped_dest['distance'].iloc[0]
-                counter += 1
+                self.EVs.work_lococation_id[index] = mapped_dest['destination_id'].iloc[0]
+                self.EVs.work_location_name[index] = self.municipalities_data.loc[self.EVs.work_lococation_id[index], 'GM_NAAM']
+                self.EVs.commute_distance[index] = mapped_dest['distance'].iloc[0]
+                index += 1
     
     def step(self):
+        # move EVs
         self.EVs.step()
     
     def update(self):
