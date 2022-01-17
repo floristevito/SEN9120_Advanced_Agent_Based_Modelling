f09b3c6bd58649300a8933fe915ba8f8af4240bd
bramboereboom <bram.boereboom@gmail.com>
2022-01-17 13:53:47 +0100

    fixed it

--- model/experiments.py
+++ model/experiments.py
@@ -3,7 +3,7 @@ import pandas as pd
 import multiprocessing as mp
 from model import EtmEVsModel
 
-profiles = pd.read_csv('../data/profiles.csv').to_dict(orient='records')
+profiles = pd.read_csv('../data/scenarios_full_test.csv').to_dict(orient='records')
 
 exp = ap.Experiment(EtmEVsModel, profiles, record=True)
 pool = mp.Pool(mp.cpu_count() - 2)
