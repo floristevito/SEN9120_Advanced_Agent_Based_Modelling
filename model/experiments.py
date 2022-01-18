a6c73254f37a99940e87e00dc0f0b4e2d10f1634
Floris Boendermaker <floris.boendermaker@gmail.com>
2022-01-18 17:00:23 +0100

    updated parallel experiments method

--- model/experiments.py
+++ model/experiments.py
@@ -1,10 +1,8 @@
 import agentpy as ap
 import pandas as pd
-import multiprocessing as mp
 from model import EtmEVsModel
 
 profiles = pd.read_csv('../data/scenarios_full_test.csv').to_dict(orient='records')
 
 exp = ap.Experiment(EtmEVsModel, profiles, record=True)
-pool = mp.Pool(mp.cpu_count() - 2)
-results = exp.run(pool)
+results = exp.run(n_jobs=-1, verbose=10)
