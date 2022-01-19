51da72d72875018d23ada78fe23708dc7a906bde
Floris Boendermaker <floris.boendermaker@gmail.com>
2022-01-19 16:30:01 +0100

    prepare vm 2

--- model/experiments.py
+++ model/experiments.py
@@ -5,6 +5,6 @@ from model import EtmEVsModel
 profiles = pd.read_csv('../data/scenarios_full.csv').to_dict(orient='records')
 
 exp = ap.Experiment(EtmEVsModel, profiles, record=True)
-results = exp.run()#n_jobs=-1, verbose=10)
+results = exp.run(n_jobs=-1, verbose=10)
 
 results.save(path='../data/experiment_results')
