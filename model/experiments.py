154acc9e9b6808be4837069fe7a089c51290fbc7
Brunohermans <b.hermans@student.tudelft.nl>
2022-01-19 16:20:45 +0100

    changed experiments.

--- model/experiments.py
+++ model/experiments.py
@@ -2,7 +2,7 @@ import agentpy as ap
 import pandas as pd
 from model import EtmEVsModel
 
-profiles = pd.read_csv('../data/profiles.csv').to_dict(orient='records')
+profiles = pd.read_csv('../data/scenarios_full.csv').to_dict(orient='records')
 
 exp = ap.Experiment(EtmEVsModel, profiles, record=True)
 results = exp.run()#n_jobs=-1, verbose=10)
