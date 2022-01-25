271e7bae3aa30a44ea4e9fc7246177d5bf6b119f
Floris Boendermaker <floris.boendermaker@gmail.com>
2022-01-25 14:35:06 +0100

    updated gif

--- model/visualization.py
+++ model/visualization.py
@@ -2,14 +2,18 @@
 def vis(model, axs):
     # create axis
     ax1, ax2 = axs
-    ax1.set_title('VTG capacity')
-    ax2.set_title('total_current_power_demand')
+    ax1.set_title('VTG Capacity')
+    ax2.set_title('Total Current Power Demand')
     
     # VTG capacity plot on first axis
     data = model.output.variables.EtmEVsModel
     data['total_VTG_capacity'] = data['total_VTG_capacity'].astype(float)
     data['total_VTG_capacity'].plot(ax=ax1) 
+    ax1.set_xlabel('Time (15min)')
+    ax1.set_ylabel('capacity (KW)')
     
     # Total current power demand on second axis
     data['total_current_power_demand'] = data['total_current_power_demand'].astype(float)
     data['total_current_power_demand'].plot(ax=ax2) 
+    ax2.set_xlabel('Time (15min)')
+    ax2.set_ylabel('capacity (KW)')
