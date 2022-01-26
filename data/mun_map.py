4ac9d1959052adac4baac2d47514006f07e7438d
Floris Boendermaker <floris.boendermaker@gmail.com>
2022-01-26 15:24:01 +0100

    more maps

--- data/mun_map.py
+++ data/mun_map.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 # local path, file too big
-results = pd.read_csv('/home/tevito/Documents/EPA/Year2/Q2/SEN9120/vm_results/vm1_results/experiment1_results/EtmEVsModel_1/variables_Municipality.csv', \
+results = pd.read_csv('/home/tevito/Documents/EPA/Year2/Q2/SEN9120/vm_results/seed_run/seed_run/EtmEVsModel_1/variables_Municipality.csv', \
     nrows=1000000)
 
 # take one run
-results = results[results['sample_id'] == 0]
+results = results[results['iteration'] == 0]
 
 # remove first week (warm-up)
 results.drop(results[(results['t'] < 672)].index, inplace=True)
